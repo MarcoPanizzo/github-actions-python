a925f567360cd97781c078b780bcdb78381c6cff
MarcoPanizzo <64210414+MarcoPanizzo@users.noreply.github.com>
2023-10-26 16:36:16 +0200

    added test 1

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)  # 2 + 3 should equal 5
         self.assertEqual(add(-1, 1), 0)  # -1 + 1 should equal 0
         self.assertEqual(add(0, 0), 0)    # 0 + 0 should equal 0
+        self.assertEqual(add(2, 2), 4)  
 
 if __name__ == "__main__":
     unittest.main()
